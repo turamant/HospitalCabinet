85336f6decbf073f48bc5a42e228c352e365aa4b
askvart <tur1amant@gmail.com>
2021-04-29 22:05:13 +0300

    gunicorn

--- Config/local_settings.py
+++ Config/local_settings.py
@@ -4,4 +4,4 @@ SECRET_KEY = 'django-insecure-2%mb7j%i2kgapxftld2b7&jym92ww5kr&$pj%otu=9vl0qi)b)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cabinet.wsgi --log-file -
